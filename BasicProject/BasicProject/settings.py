d075aa737505fc235f15a73b10a69ae14ee7e3d9
Chris Manabe <truecoz@gmail.com>
2018-04-28 02:20:25 -0700

    Adjusted index for bold type

--- BasicProject/BasicProject/settings.py
+++ BasicProject/BasicProject/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     # Add your apps here to enable them
+    'HelloDjangoApp',
+    # Other entries...
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- BasicProject/HelloDjangoApp/views.py
+++ BasicProject/HelloDjangoApp/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
+from django.shortcuts import render   # Added for this step
 
 def index(request):
-    return HttpResponse("Hello, Django!")
+    now = datetime.now()
+
+    return render(
+        request,
+        "HelloDjangoApp/index.html",  # Relative path from the 'templates' folder to the template file
+        {
+            'title' : "Hello Django",
+            'message' : "Hello Django!",
+            'content' : " on " + now.strftime("%A, %d %B, %Y at %X")
+        }
+    )
